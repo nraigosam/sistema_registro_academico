e4db5bad88da1c7855a359e58345e6e3edc5e254
Nicolas Raigosa <nicolasraigosa@gmail.com>
2023-03-28 11:34:20 -0500

    project initialization

---
+++ academicRecord/academicRecord-web/src/main/java/com/exercise/academicrecord/AcademicRecordApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.academicrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicRecordApplication.class, args);
+    }
+
+}
---
+++ academicRecord/academicRecord-web/src/test/java/com/exercise/academicrecord/AcademicRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.academicrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
